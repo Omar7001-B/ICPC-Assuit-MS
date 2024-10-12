33e650d04cc79cf6957ece4d87a1659cc543a29d
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-10-12 15:12:16 +0300

    login login added and tested

---
+++ Back/controllers/login.controller.js
@@ -0,0 +1,59 @@
+import bcrypt from "bcrypt";
+import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import { generateJWT } from "../utils/generate.JWT.js";
+
+// expexted request body
+const userDATA = {
+    gmail: "gmail or codeforces handle" , 
+    password: "password"
+}
+
+export const loginController = async (req, res) => {
+    const userData = req.body;
+
+    console.log(userData.gmail) ; 
+    const inUser = await User.findOne( { $or: [{ gmail: userData.gmail },
+                                               { codeforcesHandle: userData.gmail }] } );
+
+    if (!inUser) {
+        return res.status(400).json({
+            status: "fail",
+            message: "Password or Gmail is incorrect",
+            data: null,
+        });
+    }
+
+    const ismatch = await bcrypt.compare(userData.password, inUser.password)
+    if(ismatch){
+       
+        const token = await generateJWT({
+            firstName: inUser.firstName,
+            lastName: inUser.lastName,
+            email: inUser.email,
+            role: inUser.role,
+            codeforcesHandle: inUser.codeforcesHandle
+        });
+
+        inUser.token = token;
+        inUser.save();
+
+        return res.status(200).json({
+            status: "success",
+            message: "User logged in successfully",
+            data: inUser.token,
+        });
+    } else {
+        return res.status(400).json({
+            status: "fail",
+            message: "Password or Gmail is incorrect",
+            data: null,
+        }) ; 
+    }
+}
+
+
+// the front end must save the token in the local storage and must send it in the header of every request 
+// to auth the entered user
+// if the token is valid or not expired the user will be able to access the data
+// else the user will be redirected to the login page
