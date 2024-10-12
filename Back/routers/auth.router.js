51fb93bcab7f0b71ccd96a9e450520ad7bb9ff6e
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-10-12 15:24:42 +0300

    login api added

--- Back/routers/auth.router.js
+++ Back/routers/auth.router.js
@@ -1,14 +1,15 @@
 import express from "express";
-import { Router } from "express";
 import { signupController } from "../controllers/signup.controller.js";
+import { loginController } from "../controllers/login.controller.js";
 
 
 // signup 
-let signupRouter = express.Router();
-signupRouter.use(express.json());
+let authRouter = express.Router();
+authRouter.use(express.json());
 
 // login
 
-signupRouter.post("/api/auth/signup", signupController);
+authRouter.post("/api/auth/signup", signupController);
+authRouter.post("/api/auth/login", loginController);
 
-export default signupRouter;
+export default authRouter;
