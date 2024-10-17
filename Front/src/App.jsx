e4ac13bad0b0d329562a6ec21f640ccbceb9fa5c
Omar <benjami4n@gmail.com>
2024-10-17 18:13:12 +0300

    Added /Signup route

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,14 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Landing from './Pages/Landing/Landing';
 import Login from './Pages/Login/Login';
-const routes=createBrowserRouter([
-  {path:"/",element:<Landing/>},
-  {path:"/login",element:<Login/>}
+import SignUp from './Pages/SignUp/SignUp';
+const routes = createBrowserRouter([
+  { path: "/", element: <Landing /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <SignUp /> }
 ])
 export default function App() {
   return (
     <>
-    <RouterProvider router={routes}/>
+      <RouterProvider router={routes} />
     </>
   )
 }
