4db9e386d91c567783ec7724b3cba0c49d78db28
abdo0-0ayman <abdoayman9@gmail.com>
2024-10-17 14:32:26 +0300

    "add git userid from token"

--- Back/controllers/application.controller.js
+++ Back/controllers/application.controller.js
@@ -1,8 +1,24 @@
 import Application from "../models/applicationModel.js"; // Ensure .js extension
+import jwt from "jsonwebtoken";
 
 export const applyForTraining = async (req, res) => {
   try {
-    const { userId, trainingId } = req.body;
+    const { trainingId } = req.body;
+
+    const authHeader =
+      req.headers["authorization"] || req.headers["Authorization"];
+    if (!authHeader)
+      return res.status(401).json({ message: "No token provided" });
+    const token = authHeader.split(" ")[1];
+    console.log(token);
+    let userId;
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decodedToken);
+      userId = decodedToken.id;
+    } catch (err) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
 
     // Check if the application already exists
     const existingApplication = await Application.findOne({
@@ -22,6 +38,7 @@ export const applyForTraining = async (req, res) => {
       training: trainingId,
       status: "pending",
     });
+
     // Save application to database
     await newApplication.save();
 
