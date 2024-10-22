ddd647a5f8d8ff29c2bf488e743e08fb4f2aefcb
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-10-22 19:02:34 +0300

    protect some apis

--- Back/index.js
+++ Back/index.js
@@ -40,7 +40,7 @@ app.use("/api/emailVerification", emailVerificationRouter); // Following /api/em
 app.use(authRouter);
 app.use(codeforcesRouter);
 app.use("/Applies", applicationRouter);
-app.use("/api/users", userRouter);
+app.use("/api/users", verifyToken , userRouter); // do not delete the fucking verifyToken
 app.use("/api/Applications", applicationRouter);
 
 // Default endpoint
