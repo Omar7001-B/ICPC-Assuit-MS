332407b1c8eeae5674beefe0b1ed15b869b90a05
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-10-22 22:50:22 +0300

    bit change

--- Back/routers/application.router.js
+++ Back/routers/application.router.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import { applyForTraining , getTrainingApplications,changeStatus,getUserApplications} from '../controllers/application.controller.js'; // Ensure .js extension
+import verifyToken from '../middlewares/verifyToken.js';
+import authorizeRoles from '../middlewares/checkRole.js';
+
 
 const applicationRouter = express.Router();
 
 //admin
-applicationRouter.get('/training/:id', getTrainingApplications);
-
+applicationRouter.get('/training/:id', verifyToken , authorizeRoles("Admin")  , getTrainingApplications);
 // user application 
 applicationRouter.get('/user/:id', getUserApplications);
-
-
 applicationRouter.post('/apply', applyForTraining);
-
 applicationRouter.post('/changeStatus', changeStatus);
 // if accepted add the trainingD to the user
 
