96fbd6b08545c39a92b46b902efea3b2ab4e0194
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2024-10-22 22:49:49 +0300

    make auth over each endpoint

--- Back/routers/user.router.js
+++ Back/routers/user.router.js
@@ -13,12 +13,12 @@ import authorizeRoles from "../middlewares/checkRole.js";
 let userRouter = express.Router();
 userRouter.use(express.json());
 
-userRouter.get("/UserInfo", getUserInfo);
-userRouter.patch("/UserInfo", editUserInfo);
+userRouter.get  ("/UserInfo"  , verifyToken  ,  getUserInfo);
+userRouter.patch("/UserInfo"  , verifyToken  ,  editUserInfo);
 
-userRouter.get("/" , verifyToken , authorizeRoles("Admin") , getAllUsers); // admin only
 
-userRouter.delete("/:id", verifyToken, authorizeRoles("Admin" , "User") , deleteById); // must be middleware to check that the user is admin
+userRouter.get   ("/"   , verifyToken , authorizeRoles("Admin") , getAllUsers); // admin only
+userRouter.delete("/:id", verifyToken , authorizeRoles("Admin") , deleteById ) ; // must be middleware to check that the user is admin
  
 // we will add role manually 
 //userRouter.patch("/addRole/:id", addRole); // must be middleware to check that the user is admin
