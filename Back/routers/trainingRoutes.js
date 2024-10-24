8248b9e87c08d4f558351817fe62c64b05eebfe6
abdo0-0ayman <abdoayman9@gmail.com>
2024-10-24 13:46:37 +0300

    fix removeparticipantfromtraining

--- Back/routers/trainingRoutes.js
+++ Back/routers/trainingRoutes.js
@@ -6,9 +6,9 @@ import {
   updateTraining,
   deleteTraining,
   addParticipantsToTraining, // New route to add participants
-  removeParticipantsFromTraining,
+  removeParticipantFromTraining,
   getAllTrainingsForUser, // New route to remove participants
-  getTrainingByIdForUser
+  getTrainingByIdForUser,
 } from "../controllers/trainingController.js"; // Adjust the path based on your project structure
 import verifyToken from "../middlewares/verifyToken.js"; // Import the middleware to verify the token
 import authorizeRoles from "../middlewares/checkRole.js"; // Import the middleware to check the user's role
@@ -17,15 +17,35 @@ const router = express.Router();
 
 // Routes for training sessions
 
-router.get   ("/all"                   , verifyToken , authorizeRoles("Admin") , getAllTrainings); 
-router.get   ("/"                , verifyToken , authorizeRoles("Admin") , getTrainingById); 
-router.post  ("/"                   , verifyToken , authorizeRoles("Admin") , createTraining); 
-router.put   ("/"                , verifyToken , authorizeRoles("Admin") , updateTraining); 
-router.delete("/"                , verifyToken , authorizeRoles("Admin") , deleteTraining); 
-router.post  ("/participants"   , verifyToken , authorizeRoles("Admin") , addParticipantsToTraining); 
-router.delete("/participants"   , verifyToken , authorizeRoles("Admin") , removeParticipantsFromTraining);
+router.get("/all", verifyToken, authorizeRoles("Admin"), getAllTrainings);
+router.get("/", verifyToken, authorizeRoles("Admin"), getTrainingById);
+router.post("/", verifyToken, authorizeRoles("Admin"), createTraining);
+router.put("/", verifyToken, authorizeRoles("Admin"), updateTraining);
+router.delete("/", verifyToken, authorizeRoles("Admin"), deleteTraining);
+router.post(
+  "/participant",
+  verifyToken,
+  authorizeRoles("Admin"),
+  addParticipantsToTraining
+);
+router.delete(
+  "/participant",
+  verifyToken,
+  authorizeRoles("Admin"),
+  removeParticipantFromTraining
+);
 
-router.get("/user"              , verifyToken , authorizeRoles("User") , getTrainingByIdForUser); 
-router.get("/user/all"                  , verifyToken , authorizeRoles("User") , getAllTrainingsForUser); 
+router.get(
+  "/user",
+  verifyToken,
+  authorizeRoles("User"),
+  getTrainingByIdForUser
+);
+router.get(
+  "/user/all",
+  verifyToken,
+  authorizeRoles("User"),
+  getAllTrainingsForUser
+);
 
 export default router;
